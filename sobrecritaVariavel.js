bcc2c46a7dd21286bc5ec24e66b09e551f1a994f
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2022-10-22 13:40:42 -0300

    variveis

---
+++ sobrecritaVariavel.js
@@ -0,0 +1,17 @@
+let numero1 = 10;
+
+let soma = 0
+
+if(soma == 0){
+    //escopo local
+    let numero2 = 6;
+
+}
+else{
+    let numero1 = 10;
+}
+
+console.log(numero1)
+
+// Função
+
---
+++ variaveis.js
@@ -0,0 +1,11 @@
+var variavelNumericaNatural = 10
+
+var variavelNumericaInteira = -1
+
+var variavelNumericaReal = 0.5 // 3,1415, - 5,7
+
+
+
+let variavelNumericaNaturalLet = 10
+
+const variavelNumericaConstante = 10;
