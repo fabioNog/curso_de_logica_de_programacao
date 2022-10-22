eef47b83780cae8ec33b171a0a49abcbf1306cfe
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2022-10-22 12:52:46 -0300

    Entendendo melhor as condicionais

---
+++ condicional.js
@@ -0,0 +1,16 @@
+// condicional espera um valor logico true ou false
+// como eu iniciei o meu programa então automaticamente eu sou verdadeiro
+
+// apesar disto o if espera um valor booleano true ou false
+let a = 10;
+
+
+// para o javascript 0 é falso dentro do contexto de uma condicional
+if(a == 0){
+    console.log(a)
+    console.log("Entrei no if?")
+}else{
+    console.log("Não entrei no if")
+}
+
+
