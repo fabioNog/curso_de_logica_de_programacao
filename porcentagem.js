d836fa058d6569aa971a9e326d1bf23e2714e6b6
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2022-10-22 12:56:19 -0300

    Exemplo de porcentagem

---
+++ porcentagem.js
@@ -0,0 +1,17 @@
+
+let calcaJeans = 100
+
+let disconto = 10/100;
+
+
+// quanto vale no final esta calça com o aumento
+
+// aumento é o valor inicial mais a porcetagem aplicada ao valor inicial
+
+// valorFinalDaCalcaJeans = 100 - 10/100*100
+
+let valorFinalDaCalcaJeans = calcaJeans - disconto*calcaJeans // resultado do meu aumento aplicado no meu produto
+
+// quanto é 10% 
+
+console.log(valorFinalDaCalcaJeans);
