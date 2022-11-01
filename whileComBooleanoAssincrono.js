794f626d805ca4551b66598ae11eba16cbec2f45
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2022-11-01 19:11:56 -0300

    Subindo while switch case assicrono

---
+++ whileComBooleanoAssincrono.js
@@ -0,0 +1,34 @@
+const { promisify } = require("util");
+const readline = require("readline");
+
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question[promisify.custom] = (question) => {
+  return new Promise((resolve) => {
+    rl.question(question, resolve);
+  });
+};
+
+rl.questionSync = promisify(rl.question);
+
+(async () => {
+  let run = true;
+  while (run) {
+    let promptInput = await rl.questionSync(
+      "Node e React são a Stack Perfeita? - [ 1 => Sim | 2 => Não ]\n"
+    );
+
+    switch (Number(promptInput)) {
+      case 1:
+        console.log("Resposta certa!!!");
+        run = false;
+        rl.close();
+        break;
+      default:
+        console.log("Resposta inválida!!!");
+    }
+  }
+})();
